d3613322ccffe43ab74a5aab4d06acc19706396a
Eli Rykoff <erykoff@stanford.edu>
2021-02-22 08:45:15 -0800

    Add conda eigen includes.

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -2,6 +2,8 @@ build() {
 cd cpp
 mkdir build
 cd build
+CFLAGS="-I$CONDA_PREFIX/include/eigen3 $CFLAGS"
+CXXFLAGS="-I$CONDA_PREFIX/include/eigen3 $CXXFLAGS"
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DPYTHON_EXECUTABLE=$(command -v python) ..
 cmake --build . --config Release
 }
